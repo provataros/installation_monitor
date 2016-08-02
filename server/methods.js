edd6d896c9692a92eb70ccf7eddf20c490eea2c3
provataros <provataros@hotmail.com>
2016-08-02 09:47:12 +0300

    Change IPs

---
+++ server/methods.js
@@ -0,0 +1,12 @@
+import { Mongo } from 'meteor/mongo';
+
+Meteor.methods({
+  fixIP : function(){
+    Mongo._devices.find({gateway : "10.232.224.1"}).forEach(function(doc){
+      var ip = doc.ip.split(".");
+      var gateway = ip[0]+"."+ip[1]+"."+ip[2]+".1";
+      doc.gateway = gateway;
+      Mongo._devices.update({_id : doc._id},doc);
+    })
+  }
+})
